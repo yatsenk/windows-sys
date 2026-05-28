3859d2bba0a9986b1b79d86f75d936235c8f6724
Dmytro Yatsenko <dima0102yacenko@gmail.com>
2026-05-28 19:25:24 +0200

    All the virtual addresses of the app

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::ffi::{OsStr, c_void};
 use sysinfo::System;
 use itertools::Itertools;
 use windows_sys::Win32::{
-    Foundation::FALSE, 
+    Foundation::{CloseHandle, FALSE}, 
     System::{
         Memory::{MEMORY_BASIC_INFORMATION, VirtualQueryEx}, 
         Threading::{OpenProcess, PROCESS_ALL_ACCESS}
@@ -11,32 +11,48 @@ use windows_sys::Win32::{
 
 fn main() { 
     let system = System::new_all();
+    let process_name = OsStr::new("explorer.exe");
+    let pid = match system.processes_by_exact_name(process_name).at_most_one() {
+        Ok(Some(process)) => process.pid().as_u32(),
+        Ok(None) => {
+            println!("No process '{:?}' was found.", process_name);
+            std::process::exit(1);
+        }
+        Err(_) => {
+            println!("More than one process '{:?}' was found.", process_name);
+            std::process::exit(1);
+        }
+    };
+
+    let process_handle = unsafe {
+        OpenProcess(
+            PROCESS_ALL_ACCESS, 
+            FALSE, 
+            pid
+        )
+    };
+
     let mut mbi = MEMORY_BASIC_INFORMATION::default();
 
-    println!("PID  |  Process Name  |  Virtual Address");
-    for (pid, process) in system.processes() {
-        let process_handle = unsafe {
-            OpenProcess(
-                PROCESS_ALL_ACCESS, 
-                FALSE, 
-                pid.as_u32()
-            )
-        };
+    if process_handle != std::ptr::null_mut() {
+        let mut base_address: *const c_void = std::ptr::null_mut();
 
-        let base_address: *const c_void = std::ptr::null_mut();
+        loop {
+            let result = unsafe {
+                VirtualQueryEx(
+                    process_handle,
+                    base_address,
+                    &mut mbi,
+                    size_of::<MEMORY_BASIC_INFORMATION>(),
+                )
+            };
 
-        let result = unsafe {
-            VirtualQueryEx(
-                process_handle,
-                base_address,
-                &mut mbi,
-                size_of::<MEMORY_BASIC_INFORMATION>(),
-            )
-        };
-
-        if result != 0 {
-            let base_address = base_address.wrapping_add(mbi.RegionSize);
-            print!("[{}] {:?} {:?}\n", pid, process.name(), base_address);
+            if result == 0 { break; }
+            
+            print!("[{}] {:?} {:?}\n", pid, process_name, base_address);
+            base_address = base_address.wrapping_add(mbi.RegionSize);
         }
+        
+        unsafe { CloseHandle(process_handle) };
     }
 }
